353bbc5ee0dca8eec754ae2525142dba13c643ef
Josh Goldberg <josh@fullscreenmario.com>
2019-03-06 21:33:21 -0500

    Testing: intentionally failing Mocha test (without package changes)

--- src/tests/AstUtilsTests.ts
+++ src/tests/AstUtilsTests.ts
@@ -4,9 +4,9 @@ import * as chai from 'chai';
 describe('AstUtils', (): void => {
     describe('isJQuery', (): void => {
         it('should match expected strings', (): void => {
-            chai.expect(AstUtils.isJQuery('$')).to.equal(true, 'short form');
-            chai.expect(AstUtils.isJQuery('jQuery')).to.equal(true, 'typical long form');
-            chai.expect(AstUtils.isJQuery('jquery')).to.equal(true, 'another long form');
+            chai.expect(AstUtils.isJQuery('$')).to.equal(true, 'HELLO MY BABY');
+            chai.expect(AstUtils.isJQuery('jQuery')).to.equal(true, 'HELLO MY HONEY');
+            chai.expect(AstUtils.isJQuery('jquery')).to.equal(true, 'HELLO MY RAGTIME GAL');
         });
         it('should not match unexpected strings', (): void => {
             chai.expect(AstUtils.isJQuery('S')).to.equal(false, 'Not jquery');
